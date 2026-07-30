1ad1b9151522c28b7618c6a3580a812a4af57fa9
JJ-Shapiro <joebuildsdrones@gmail.com>
2026-07-30 18:43:11 +0100

    first commit

---
+++ Quiet.mjs
@@ -0,0 +1,39 @@
+import express from "express"
+import fs from "fs"
+
+
+const app = express()
+const port = 6464;
+const helloworld = "hello world!";
+
+const filePath = "/Users/joe/Documents/Quiet/muse/"
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req, res) => {
+
+  res.send(helloworld);
+
+  console.log("GET");
+});
+
+app.post("/LikeSilenceButNotReallySilence", (req, res) => {
+  const data = req.body;
+  const muse = req.body.muse;
+
+  const filePathUnique = filePath + Date() + ".txt";
+
+  console.log(filePathUnique);
+  
+  console.log("url: " + req.url);
+  console.log("muse: " + muse);
+
+  fs.writeFileSync(filePathUnique, muse, "utf-8");
+
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
